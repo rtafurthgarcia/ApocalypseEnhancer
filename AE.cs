3509154897f79ebaefa3e8ca8f6b160fc144bdb6
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2025-12-31 16:31:32 +0100

    Created the base template

---
+++ AE.cs
@@ -0,0 +1,18 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace ApocalypseEnhancer
+{
+    public class AE : IUserMod
+    {
+        public string Name
+        {
+            get { return "Apocalypse Enhancer v0.0.1"; }
+        }
+
+        public string Description
+        {
+            get { return "Enhancing visual effects in destroyed cities and automatically disabling destroyed public buidlings "; }
+        }
+    }
+}
