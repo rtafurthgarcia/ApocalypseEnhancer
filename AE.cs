51d78b84a158ba6586441db21ef1f0b975e4fd93
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-01-01 12:19:58 +0100

    Rajout du chargement. Devrait être chargé automatiquement dans le jeu et afficher un message de debug

--- AE.cs
+++ AE.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 namespace ApocalypseEnhancer
 {
+    #region Mod Definition
+    /// <summary>
+    /// Provide description of the mod to the game, this is shown during mod loading screen.
+    /// </summary>
     public class AE : IUserMod
     {
         public string Name
@@ -14,5 +18,29 @@ namespace ApocalypseEnhancer
         {
             get { return "Enhancing visual effects in destroyed cities and automatically disabling destroyed public buidlings "; }
         }
+
+        public void OnSettingsUI(UIHelperBase helper) {
+            UIHelperBase group = helper.AddGroup(this.Name);
+        }
     }
+    #endregion
+
+    #region Mod Behavior
+    /// <summary>
+    /// Will first look at all roads, and if they are flooded, will turn off their street lights.
+    /// Will also look at all public buildings, and turn them off if they are flooded or destroyed
+    /// </summary>
+    public class AELoader : LoadingExtensionBase
+    {
+        /// <summary>
+        /// This event is triggerred when a level is loaded
+        /// </summary>
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            base.OnLevelLoaded(mode);
+
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "AE Loaded");
+        }
+    }
+    #endregion
 }
